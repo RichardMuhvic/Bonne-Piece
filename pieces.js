0de0fdb050e707ce999209ee25d2df82d117c071
Richard <richardmuhvic@gmail.com>
2022-09-14 22:20:20 +0200

    ajout de la boucle for

--- pieces.js
+++ pieces.js
@@ -2,40 +2,6 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Création des éléments et remplissage du texte ou de la source de l'image: 
-const ampoule = pieces[0];
-
-const imageElement = document.createElement("img");
-imageElement.src = ampoule.image;
-
-const nomElement = document.createElement("h2");
-nomElement.innerText = ampoule.nom;
-
-const prixElement = document.createElement("p");
-prixElement.innerText = "Prix " + ampoule.prix + " €"
-	+ " ("
-	+ (ampoule.prix < 35 ? "€" : "€€€")
-	+ ")";
-
-const categorieElement = document.createElement("p");
-categorieElement.innerText = ampoule.categorie ?? "(aucune catégorie)";
-
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = ampoule.description ?? "Pas de description pour le moment.";
-
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = ampoule.disponibilite ? "en stock" : "rupture de stock";
-
-//Récupération de l'élément du DOM qui accueillera les fiches :
-const sectionFiches = document.querySelector(".fiches");
-
-// Ajout des éléments créés dans le DOM
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(disponibiliteElement);
-sectionFiches.appendChild(descriptionElement);
 
 for (let i = 0; i < pieces.length; i++) {
 	// Récupération de l'élément du DOM qui accueillera les fiches
@@ -53,6 +19,33 @@ for (let i = 0; i < pieces.length; i++) {
 
 	// Idem pour le nom, le prix et la catégorie...
 
+	const nomElement = document.createElement("h2");
+	nomElement.innerText = pieces[i].nom;
+	pieceElement.appendChild(nomElement);
+
+	const prixElement = document.createElement("p");
+	prixElement.innerText = "Prix " + pieces[i].prix + " €"
+		+ " ("
+		+ (pieces[i].prix < 35 ? "€" : "€€€")
+		+ ")";
+
+	const categorieElement = document.createElement("p");
+	categorieElement.innerText = pieces[i].categorie ?? "(aucune catégorie)";
+
+	const descriptionElement = document.createElement("p");
+	descriptionElement.innerText = pieces[i].description ?? "Pas de description pour le moment.";
+
+	const disponibiliteElement = document.createElement("p");
+	disponibiliteElement.innerText = pieces[i].disponibilite ? "en stock" : "rupture de stock";
+
+	// Ajout des éléments créés dans le DOM
+	sectionFiches.appendChild(imageElement);
+	sectionFiches.appendChild(nomElement);
+	sectionFiches.appendChild(prixElement);
+	sectionFiches.appendChild(categorieElement);
+	sectionFiches.appendChild(disponibiliteElement);
+	sectionFiches.appendChild(descriptionElement);
+	
 	// On rattache la balise article au body
 	document.body.appendChild(pieceElement);
 }
